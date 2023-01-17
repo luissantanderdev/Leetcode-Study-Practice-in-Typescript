1fa293f2e2f4a33ae67830bbd6ad5e2b43dd425c
codedinsound <107017540+codedinsound@users.noreply.github.com>
2023-01-16 17:11:02 -0700

    1 file modified

--- Data Structures and Algorithms/Graph Algorithms/index.ts
+++ Data Structures and Algorithms/Graph Algorithms/index.ts
@@ -1,16 +1,30 @@
 import { Graph } from '../../Data Structures and Algorithms/Models';
 
-class DiagonalSearchAlgorithm implements Graph {
-  traverse(matrix) {}
-}
-
 class RightDiagonal implements Graph {
-  traverse(matrix) {
+  traverse(matrix: any) {
     for (let i = 0; i < matrix.length - 1; i++) {
       for (let j = i; j < i + 1; j++) {
         console.log(matrix[i][j]);
       }
     }
+  }
+}
+
+// MARK: DFS - Depth First Search
+class DepthFirstSearchAlgorithm implements Graph {
+  directions: number[][] = [
+    [-1, 0], // Up
+    [0, 1], // Right
+    [1, 0], // Down
+    [0, -1], // Left
+  ];
+
+  traversalDFS(matrix: number[][]) {
+    const seen = new Array(matrix.length).fill(0);
+  }
+
+  traverse(matrix: any) {
+    this.traversalDFS(matrix);
   }
 }
 
@@ -36,11 +50,23 @@ class GraphManager {
       [17, 18, 19, 20],
     ];
 
+    const testMatrix = [
+      [1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10],
+      [11, 12, 13, 14, 15],
+      [16, 17, 18, 19, 20],
+    ];
+
     const algorithm: string = 'right';
 
     switch (algorithm) {
       case 'right':
         this.setGraphAlgorithm(new RightDiagonal()).traverse(matrix);
+        break;
+      case 'dfs':
+        this.setGraphAlgorithm(new DepthFirstSearchAlgorithm()).traverse(
+          matrix
+        );
         break;
       default:
     }
