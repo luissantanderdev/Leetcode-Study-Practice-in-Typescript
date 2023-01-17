458ca5681ea35ec5fd039838f9c976e572fc0cc4
codedinsound <107017540+codedinsound@users.noreply.github.com>
2023-01-16 20:53:07 -0700

    1 file modified

--- Data Structures and Algorithms/Appendix Refresher/What is Recursion Examples/index.ts
+++ Data Structures and Algorithms/Appendix Refresher/What is Recursion Examples/index.ts
@@ -3,6 +3,11 @@
 // 2. Identify the recursive case
 // 3. Get closer and closer and return when needed.
 
+// Recursion Benefits and Draw Backs
+// 1. DRY Readability
+// 2. But adds a large stack which adds space complexity.
+// 3. Very helpful for things where you don't know how deep something is like in trees.wh
+
 // MARK: Tail Recursion
 // NOTE: Tail Recursion: A call is tail-recursive if nothing has to be done after the call returns i.e. when the call returns, the returned value is immediately returned from the calling function
 function tail(n: number) {
