7b594e740b0cd4b41cab17e2da3e8b05227b5650
codedinsound <107017540+codedinsound@users.noreply.github.com>
2023-01-17 14:03:52 -0700

    1 file modified

--- Data Structures and Algorithms/Appendix Refresher/What is Recursion Examples/index.ts
+++ Data Structures and Algorithms/Appendix Refresher/What is Recursion Examples/index.ts
@@ -8,6 +8,17 @@
 // 2. But adds a large stack which adds space complexity.
 // 3. Very helpful for things where you don't know how deep something is like in trees.wh
 
+
+/**
+ *  Everytime you are using a tree or converting something into a tree, consider recursion. 
+ *  1. Divide into a number of subproblems that are smaller instances of the same problem. 
+ *  2. Each instance of the subproblem is identical in nature. 
+ *  3. The solutions of each subproblem can be combined to solve the problem at hand. 
+ * 
+ */
+
+
+
 // MARK: Tail Recursion
 // NOTE: Tail Recursion: A call is tail-recursive if nothing has to be done after the call returns i.e. when the call returns, the returned value is immediately returned from the calling function
 function tail(n: number) {
