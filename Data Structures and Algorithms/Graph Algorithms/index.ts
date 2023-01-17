dbb4e9843769fb0f48336a374b9c73c863336738
codedinsound <107017540+codedinsound@users.noreply.github.com>
2023-01-16 19:52:58 -0700

    1 file modified

--- Data Structures and Algorithms/Graph Algorithms/index.ts
+++ Data Structures and Algorithms/Graph Algorithms/index.ts
@@ -46,7 +46,7 @@ class DepthFirstSearchAlgorithm implements Graph {
   }
 
   traversalDFS(matrix: number[][]): number[] {
-    const seen = new Array(matrix.length).fill(0).map(() => {
+    const seen: boolean[][] = new Array(matrix.length).fill(0).map(() => {
       return new Array(matrix[0].length).fill(false);
     });
 
@@ -59,6 +59,60 @@ class DepthFirstSearchAlgorithm implements Graph {
 
   traverse(matrix: any) {
     this.traversalDFS(matrix);
+  }
+}
+
+// MARK: BFS - Breath First Search
+// NOTE: Breath First Search utilizes a diamond pattern of searching utilizign a queue data structure ^ > v < going in these directions.
+// Time: O(n)
+// Space: O(n);
+class BreathFirstSearch implements Graph {
+  directions: number[][] = [
+    [-1, 0], // up ^
+    [0, 1], // right
+    [1, 0], // down
+    [0, -1], // left
+  ];
+
+  traverseBFS(matrix: number[][]): number[] {
+    const seen: boolean[][] = new Array(matrix.length)
+      .fill(0)
+      .map(() => new Array(matrix[0].length).fill(false));
+
+    const values = [];
+    const queue = [[0, 0]];
+
+    while (queue.length) {
+      const currentPos = queue.shift();
+      const row = currentPos[0];
+      const col = currentPos[1];
+
+      if (
+        row < 0 ||
+        row >= matrix.length ||
+        col < 0 ||
+        col >= matrix[0].length ||
+        seen[row][col]
+      )
+        continue;
+
+      seen[row][col] = true;
+      values.push(matrix[row][col]);
+
+      for (let i = 0; i < this.directions.length; i++) {
+        const currentDir = this.directions[i];
+        queue.push([row + currentDir[0], col + currentDir[1]]);
+      }
+    }
+    return values;
+  }
+
+  traverse(matrix: any) {
+    this.traverseBFS(matrix);
+  }
+
+  static newInstance(): BreathFirstSearch {
+    return new BreathFirstSearch();
   }
 }
 
@@ -77,21 +131,13 @@ class GraphManager {
 
   static test() {
     const matrix = [
-      [1, 2, 3, 4],
-      [5, 6, 7, 8],
-      [9, 10, 11, 12],
-      [13, 14, 15, 16],
-      [17, 18, 19, 20],
-    ];
-
-    const testMatrix = [
       [1, 2, 3, 4, 5],
       [6, 7, 8, 9, 10],
       [11, 12, 13, 14, 15],
       [16, 17, 18, 19, 20],
     ];
 
-    const algorithm: string = 'dfs';
+    const algorithm: string = 'bfs';
 
     switch (algorithm) {
       case 'right':
@@ -99,6 +145,11 @@ class GraphManager {
         break;
       case 'dfs':
         this.setGraphAlgorithm(new DepthFirstSearchAlgorithm()).traverse(
+          matrix
+        );
+        break;
+      case 'bfs':
+        this.setGraphAlgorithm(BreathFirstSearch.newInstance()).traverse(
           matrix
         );
         break;
