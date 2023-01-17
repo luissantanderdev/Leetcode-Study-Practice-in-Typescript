521029558a4db90b6eb36abd51a5496e5c1f16f5
codedinsound <107017540+codedinsound@users.noreply.github.com>
2023-01-16 20:26:48 -0700

    1 file modified

--- Data Structures and Algorithms/Appendix Refresher/What is Recursion Examples/index.ts
+++ Data Structures and Algorithms/Appendix Refresher/What is Recursion Examples/index.ts
@@ -3,6 +3,24 @@
 // 2. Identify the recursive case
 // 3. Get closer and closer and return when needed.
 
+// MARK: Tail Recursion
+// NOTE: Tail Recursion: A call is tail-recursive if nothing has to be done after the call returns i.e. when the call returns, the returned value is immediately returned from the calling function
+function tail(n: number) {
+  if (n === 1) return;
+  else console.log(n);
+
+  tail(n - 1);
+}
+
+// MARK: Head Recursion
+// NOTE: Head Recursion: A call is head-recursive when the first statement of the function is the recursive call.
+function head(n: number) {
+  if (n === 0) return;
+  else head(n - 1);
+
+  console.log(n);
+}
+
 // MARK: Simple Recursive Counter
 let counter: number = 0;
 function inception() {
@@ -12,10 +30,3 @@ function inception() {
   counter++;
   inception();
 }
-
-// MARK: Tail Recursion
-// NOTE: Tail Recursion: A call is tail-recursive if nothing has to be done after the call returns i.e. when the call returns,
-//       the returned value is immediately returned from the calling function
-
-// MARK: Head Recursion
-// NOTE: Head Recursion: A call is head-recursive when the first statement of the function is the recursive call.
